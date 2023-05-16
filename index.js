eccad1970c69791ef757a6f0bb64bb15f5e5a246
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-05-16 12:50:37 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,16 +28,132 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
+
+
+
+    //services
+    
+    //get multiple data from database
+
+    app.get('/services', async(req, res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    //get single data from database
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+
+        // we use options to specify the datas we want
+
+        const options = {
+            
+            projection: { title: 1, price: 1, service_id: 1, img: 1 },
+          };
+      
+
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+    })
+
+
+    //bookings
+
+    //send data to server and insert it to database
+
+    app.post('/bookings', async (req, res)=>{
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+    })
+
+    // get some data from database
+
+    app.get('/bookings', verifyJWT, async(req, res)=>{
+      const decoded = req.decoded;
+        console.log('come back after verify', decoded);
+        if(decoded.email  !== req.query.email){
+          return res.status(403).send({error: true, message: 'unauthorized token'})
+        }
+        let query = {};
+        if (req.query?.email){
+            query= {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    // update booking
+
+    app.patch('/bookings/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = { _id : new ObjectId(id)};
+        const updatedBooking = req.body;
+        console.log(updatedBooking);
+        // create a document that sets the plot of the movie
+    const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // delete booking
+
+    app.delete('/bookings/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)};
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+    })
+    //JWT
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({token});
+
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
